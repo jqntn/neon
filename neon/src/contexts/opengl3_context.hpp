2b612148643904d4a5be65098234dab4e4635264
Julien QUENTIN <jqntn88@gmail.com>
2024-05-25 00:59:01 +0200

    init

---
+++ neon/src/contexts/opengl3_context.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+int
+opengl3_init();
---
+++ neon/src/neon.cpp
@@ -0,0 +1,9 @@
+﻿#include "contexts/opengl3_context.hpp"
+#include "contexts/vulkan_context.hpp"
+
+int
+main()
+{
+  return opengl3_init();
+  return vulkan_init();
+}
